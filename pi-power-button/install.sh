f12eb86860eae702c5f917d4872ec8a822cb0486
Ago <99293615+therealago@users.noreply.github.com>
2022-02-09 01:02:13 +0100

    Update install.sh

--- pi-power-button/install.sh
+++ pi-power-button/install.sh
@@ -7,11 +7,11 @@ set -e
 cd "$(dirname "$0")/.."
 
 echo "=> Installing shutdown listener...\n"
-sudo cp listen-for-shutdown.py /usr/local/bin/
+sudo cp listen-for-shutdown.py /usr/local/bin
 sudo chmod +x /usr/local/bin/listen-for-shutdown.py
 
 echo "=> Starting shutdown listener...\n"
-sudo cp listen-for-shutdown.sh /etc/init.d/
+sudo cp listen-for-shutdown.sh /etc/init.d
 sudo chmod +x /etc/init.d/listen-for-shutdown.sh
 
 sudo update-rc.d listen-for-shutdown.sh defaults
